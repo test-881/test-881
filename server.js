7f46ea2ea0a00d6cd90c7b72b6e45637dc029c5c
Igor Zhivilo <warolv@gmail.com>
2020-02-29 19:18:13 +0200

    update Jenkinsfile

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World 4');
+  res.send('Hello World');
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+module.exports = app;
